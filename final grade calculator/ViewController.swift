69c52c423de9a61b0b92c86950f0d37e90f25aa3
Esmay <Molinaesme729@gmail.com>
2022-09-29 10:55:08 -0600

    created story board

--- final grade calculator/ViewController.swift
+++ final grade calculator/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var krean: UILabel!
+    
+    @IBOutlet var lol: UIView!
+    
+    @IBOutlet weak var idfc: UITextField!
+    
+    
+    @IBOutlet weak var gth: UITextField!
+    
+    @IBOutlet weak var yolo: UITextField!
+
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func function(_ sender: Any) {
+        
+        print(lmao.text)
+        
+        print(yolo.text)
+        
+        
+        
+        
+    }
+    
 
 }
 
