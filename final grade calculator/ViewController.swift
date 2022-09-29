a63c7c468c54f5bf27ba55ae6059a4152d40392c
Esmay <Molinaesme729@gmail.com>
2022-09-29 09:36:09 -0600

    Initial Commit

---
+++ final grade calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  final grade calculator
+//
+//  Created by Molina, Yadira - Student on 9/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
