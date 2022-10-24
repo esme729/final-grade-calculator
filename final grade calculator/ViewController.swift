ed32a05a840e3a897425bebe3937da11003cde96
Esmay <Molinaesme729@gmail.com>
2022-10-24 10:33:31 -0600

    added double variables mvp

--- final grade calculator/ViewController.swift
+++ final grade calculator/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var krean: UILabel!
     
-
+    
     
     
     @IBOutlet weak var fly: UITextField!
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var gth: UITextField!
     
     @IBOutlet weak var yolo: UITextField!
-
+    
     @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
@@ -30,26 +30,31 @@ class ViewController: UIViewController {
     
     
     @IBAction func function(_ sender: Any) {
-    
-var text1string = ""
-var dubble1=0.0
-var text2string = ""
-var dubble2=0.0
-var text3string = ""
-var dubble3=0.0
         
-        text1string = fly.text ?? ""
-        text2string = gth.text ?? ""
+        var text1string = ""
+        var dubble1=0.0
+        var text2string = ""
+        var dubble2=0.0
+        var text3string = ""
+        var dubble3=0.0
+        
+        text2string = fly.text ?? ""
+        text1string = gth.text ?? ""
         text3string = yolo.text ?? ""
-       
+        
         //assign double variables from textfields
         dubble1 = Double(text1string) ?? 0.0
+        dubble2 = Double(text2string) ?? 0.0
+        dubble3 = Double(text3string) ?? 0.0
         
+        var pie = 100-dubble3
+var currentgradeweight=dubble1*pie/100
+  
+var examcount = dubble2-currentgradeweight
         
+        var exam = examcount/dubble3*100
         
-        
+        krean.text="\(exam)"
     }
     
-
 }
-
